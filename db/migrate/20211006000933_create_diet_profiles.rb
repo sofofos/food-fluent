0c91d00dbcf47e6e95fe8a88658708c49d790358
Sofia Alvarez <sofia.alva.gonza@gmail.com>
2021-10-05 20:10:23 -0400

    added diet profile model, ready to merge with implement-devise-user branch

---
+++ db/migrate/20211006000933_create_diet_profiles.rb
@@ -0,0 +1,10 @@
+class CreateDietProfiles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :diet_profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :healthLabel, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
