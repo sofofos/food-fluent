6cb10e2f3b5d8ed0be5b4b4b6cf823ee2ed6a842
veronica <verovargaszaraza@gmail.com>
2021-10-12 20:43:22 -0400

    added basic controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,22 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: %i[show]
   def index
+    @restaurants = Restaurant.all.order(created_at: :desc)
+    # find_health_tag
   end
 
-  def show
+  def show; end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def find_health_tag
+    users_tags = user.
+
+    @dishes = Dish.all
+    @restaurants = Restaurant.all.reject{ |restaurant| restaurant.dishes != users_tags}
   end
 end
