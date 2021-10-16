7ce38dbb2e1fda7395b80f01628cde3259d283f3
veronica <verovargaszaraza@gmail.com>
2021-10-16 12:36:03 -0400

    creates several users at the same time

--- app/controllers/diet_profiles_controller.rb
+++ app/controllers/diet_profiles_controller.rb
@@ -5,19 +5,23 @@ class DietProfilesController < ApplicationController
   end
 
   def create
-    @diet_profile = DietProfile.new(diet_profile_params)
-    @diet_profile.user = current_user
-    authorize @diet_profile
-    if @diet_profile.save
-      redirect_to root_path
-    else
-      render :new
+    diet_profile_params[:health_label_id].each do |id|
+      # raise
+      @diet_profile = DietProfile.new
+      @diet_profile.user = current_user
+      @diet_profile.health_label = HealthLabel.find(id.to_i)
+      authorize @diet_profile
+      render :new && return unless @diet_profile.save
     end
+    
+    redirect_to root_path
   end
 
   private
 
   def diet_profile_params
-    params.require(:diet_profile).permit(:health_labels)
+    params.require(:diet_profile).permit(health_label_id: [])
+
+    # health_label_id"=>["", "209"]} permitted: false>, "commit"=>"Submit list", "controller"=>"diet_profiles", "action"=>"create"} permitted: false>
   end
 end
