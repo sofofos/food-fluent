141a0823f30ad417d0ae3c525d5b8cda29d73bb0
veronica <verovargaszaraza@gmail.com>
2021-10-19 20:45:02 -0400

    doing my best

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -17,7 +17,15 @@ class RestaurantsController < ApplicationController
   end
 
   def find_health_label
-    users_health_label = current_user.diet_profiles.map(&:health_label)
+    # users = current_user + selected_friends
+    # users.each do |user|
+    #   users_health_label = user.diet_profiles.map(&:health_label)
+    #   @dishes = Dish.all.select { |dish| (users_health_label - dish.health_labels).empty? }
+    # end
+    # make sure a restaurant has a dish for each person?
+    # make that @restaurants
+
+    users_health_label = current_user.health_labels
     @dishes = Dish.all.select { |dish| (users_health_label - dish.health_labels).empty? }
     @restaurants = @dishes.map(&:restaurant).uniq
   end
