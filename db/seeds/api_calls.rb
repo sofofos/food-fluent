4266cdb64d576d4af60fbaf3275c512c158d04df
Sofia Alvarez <sofia.alva.gonza@gmail.com>
2021-10-20 23:12:13 -0400

    another bug reading from files... tbc tomorrow

--- db/seeds/api_calls.rb
+++ db/seeds/api_calls.rb
@@ -2,9 +2,10 @@ require 'json'
 require 'rest-client'
 require 'faker'
 
-require './db/seeds.rb'
-
 dish_type = Dish.dish_types
+
+index_for_datasets = Dish.all.count / 80
+index_for_datasets == 0 ? idx = "_" : idx = index_for_datasets + 1
 
 def api_call(dish_type, opt = "")
   base = "https://api.edamam.com/api/recipes/v2?type=public"
@@ -22,24 +23,21 @@ end
 
 # first request: starters
 data_hash = api_call(dish_type[:starter])
-File.write("storage/starters#{@idx}.json", JSON.dump(data_hash))
+File.write("storage/starters#{idx}.json", JSON.dump(data_hash))
 
 # second request: main courses
 data_hash = api_call(dish_type[:main])
-File.write("storage/mains#{@idx}.json", JSON.dump(data_hash))
+File.write("storage/mains#{idx}.json", JSON.dump(data_hash))
 
 #  third request: salads
 data_hash = api_call(dish_type[:salad])
-File.write("storage/salads#{@idx}.json", JSON.dump(data_hash))
+File.write("storage/salads#{idx}.json", JSON.dump(data_hash))
 
 #  fourth request: desserts
 fruit = Faker::Food.fruits
 data_hash = api_call(dish_type[:dessert], fruit)
-File.write("storage/desserts#{@idx}.json", JSON.dump(data_hash))
+File.write("storage/desserts#{idx}.json", JSON.dump(data_hash))
 puts "Today's desserts are inspired by #{fruit}! Enjoy"
-
-# Updating index for next api_call / restaurant
-@idx += 1
 
 puts "Ensuring I am not executed again for 30 seconds, pls wait..."
 sleep(30)
