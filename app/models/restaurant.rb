d072e0450c4baa14db3fe238b4b2f3d1f663a270
veronica <verovargaszaraza@gmail.com>
2021-10-23 15:34:50 -0400

    see dishes for each restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   has_many :dishes, dependent: :destroy
+
+  def dishes_for(user)
+    dishes.select { |dish| (user.health_labels - dish.health_labels).empty? }
+  end
 end
