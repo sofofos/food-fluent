c13aa5bb6633690054d75f34653e352d1b912073
Sofia Alvarez <sofia.alva.gonza@gmail.com>
2021-10-23 11:29:52 -0400

    improved seed file

--- db/seeds/make_dishes.rb
+++ db/seeds/make_dishes.rb
@@ -1,35 +1,47 @@
 # make the dishes for restaurants in db/seeds.rb
 require './db/seeds/load_files'
 
-ERRORS = [NoMethodError, ArgumentError]
+labels = [ "Keto-Friendly",
+ "Kosher",
+ "Low Sugar",
+ "Mediterranean",
+ "Paleo",
+ "Pescatarian",
+ "Sugar-Conscious",
+ "Vegan",
+ "Vegetarian" ]
+
+ ERRORS = [NoMethodError, ArgumentError]
 
 def make_dish(restaurant, dish_data, i)
-# if index for dishes reaches max dataset entries:
+  # if index for dishes reaches max dataset entries:
   i > 20 ? j = i - 20 : j = i
-  @count_errors = 0
 
-  @dish = Dish.new(
-    name: dish_data["hits"][j]["recipe"]["label"],
-    restaurant: restaurant,
-    img_url: dish_data["hits"][j]["recipe"]["image"],
-    dish_type: dish_data["hits"][j]["recipe"]["dishType"][0]
-  )
-  puts "created a dish: #{@dish.name}"
-rescue *ERRORS
-  j += 1
-  @count_errors += 1
-else
-  @dish.save!
-  make_dish_labels(dish_data, j)
+  if dish_data["hits"][j]["recipe"]["dishType"].count == 1
+    @dish = Dish.new(
+      name: dish_data["hits"][j]["recipe"]["label"],
+      restaurant: restaurant,
+      img_url: dish_data["hits"][j]["recipe"]["image"],
+      dish_type: dish_data["hits"][j]["recipe"]["dishType"][0]
+      )
+    puts "created a dish: #{@dish.name}"
+    @dish.save!
+    make_dish_labels(dish_data, dish, j)
+  end
 end
 
-def make_dish_labels(dish_data, j)
-  dish_health_labels = dish_data["hits"][j]["recipe"]["healthLabels"]
+def make_dish_labels(dish_data, dish, j)
+  health_labels = dish_data["hits"][j]["recipe"]["healthLabels"]
+
+  dish_health_labels = health_labels.select{ |label| labels.includes?(label) }
+
   dish_health_labels.each do |label|
-    dish_label = HealthLabel.create!(name: label)
-    DishHealthLabel.new(
+    dish_label = HealthLabel.create(name: label)
+    dhl = DishHealthLabel.new(
       dish_id: @dish.id,
       health_label_id: dish_label.id
-    )
+      )
+    dish_label.save!
+    dhl.save!
   end
 end
