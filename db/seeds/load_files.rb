f89b9d337179f53160e9c955e5e6d9ef0308ed2c
Sofia Alvarez <sofia.alva.gonza@gmail.com>
2021-10-23 11:52:29 -0400

    fixed seed file for dish health labels

--- db/seeds/load_files.rb
+++ db/seeds/load_files.rb
@@ -9,17 +9,19 @@ end
 
 def load_files
   puts "I am in load_files.rb"
-  starters = File.read("storage/starters#{@idx}.json")
-  @starters_hash = JSON.parse(starters)
+  # starters = File.read("storage/starters#{@idx}.json")
+  # @starters_hash = JSON.parse(starters)
 
   salad = File.read("storage/salads#{@idx}.json")
+  puts "salads #{@idx} loaded"
   @salad_hash = JSON.parse(salad)
 
   main = File.read("storage/mains#{@idx}.json")
+  puts "loading mains#{@idx}"
   @main_hash = JSON.parse(main)
 
-  desserts = File.read("storage/desserts#{@idx}.json")
-  @desserts_hash = JSON.parse(desserts)
+  # desserts = File.read("storage/desserts#{@idx}.json")
+  # @desserts_hash = JSON.parse(desserts)
 rescue Errno::ENOENT
   Rake::Task['db:seed:api_calls'].invoke
   retry
--- db/seeds/make_dishes.rb
+++ db/seeds/make_dishes.rb
@@ -1,7 +1,7 @@
 # make the dishes for restaurants in db/seeds.rb
 require './db/seeds/load_files'
 
-labels = [ "Keto-Friendly",
+LABELS = [ "Keto-Friendly",
  "Kosher",
  "Low Sugar",
  "Mediterranean",
@@ -15,17 +15,18 @@ labels = [ "Keto-Friendly",
 
 def make_dish(restaurant, dish_data, i)
   # if index for dishes reaches max dataset entries:
-  i > 20 ? j = i - 20 : j = i
+  i >= 20 ? j = i - 20 : j = i
+  puts "#{j}"
 
-  if dish_data["hits"][j]["recipe"]["dishType"].count == 1
-    @dish = Dish.new(
+  if j < 20 && dish_data["hits"][j]["recipe"]["dishType"].count == 1
+    dish = Dish.new(
       name: dish_data["hits"][j]["recipe"]["label"],
       restaurant: restaurant,
       img_url: dish_data["hits"][j]["recipe"]["image"],
       dish_type: dish_data["hits"][j]["recipe"]["dishType"][0]
       )
-    puts "created a dish: #{@dish.name}"
-    @dish.save!
+    puts "created a dish: #{dish.name}"
+    dish.save!
     make_dish_labels(dish_data, dish, j)
   end
 end
@@ -33,12 +34,12 @@ end
 def make_dish_labels(dish_data, dish, j)
   health_labels = dish_data["hits"][j]["recipe"]["healthLabels"]
 
-  dish_health_labels = health_labels.select{ |label| labels.includes?(label) }
+  dish_health_labels = health_labels.select{ |label| LABELS.include?(label) }
 
   dish_health_labels.each do |label|
     dish_label = HealthLabel.create(name: label)
     dhl = DishHealthLabel.new(
-      dish_id: @dish.id,
+      dish_id: dish.id,
       health_label_id: dish_label.id
       )
     dish_label.save!
