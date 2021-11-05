5ac7a136401ee76d782fd12a652912228c0a22fe
Sofia Alvarez <sofia.alva.gonza@gmail.com>
2021-11-04 20:31:30 -0400

    re-enabled random_avatar user method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # after_create :random_avatar
+  after_create :random_avatar
 
   has_friendship
 
   has_many :diet_profiles, dependent: :destroy
   has_many :health_labels, through: :diet_profiles
 
-  # def random_avatar
-  #   avatars = ["avatars/chris.png", "avatars/sofia.png", "avatars/vivi.png"]
-  #   self.avatar_path = avatars.sample if self.avatar_path.nil?
-  #   self.save!
-  # end
-
+  def random_avatar
+    avatars = ["avatars/chris.png", "avatars/sofia.png", "avatars/vivi.png"]
+    self.avatar_path = avatars.sample if self.avatar_path.nil?
+    self.save!
+  end
 end
